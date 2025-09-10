b599f552e4967de4cb1da5bfea302d72bd3cd196
vivesing <vivek.singh1694@gmail.com>
2025-09-10 09:54:06 +0530

    [microservices] Adding the readiness and liveliness probes

--- accounts/src/main/java/com/viv/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/viv/accounts/AccountsApplication.java
@@ -22,4 +22,5 @@ public class AccountsApplication {
 		SpringApplication.run(AccountsApplication.class, args);
 	}
 
+	
 }
--- loans/src/main/java/com/viv/loans/LoansApplication.java
+++ loans/src/main/java/com/viv/loans/LoansApplication.java
@@ -15,23 +15,7 @@ import com.viv.loans.dto.LoansContactInfoDto;
 @SpringBootApplication
 @EnableConfigurationProperties(value = LoansContactInfoDto.class)
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Loans microservice REST API Documentation",
-				description = "Loans microservice REST API Documentation",
-				version = "v1",
-				contact = @Contact(
-						name = "John Doe",
-						email = "john@example.com"
-				),
-				license = @License(
-						name = "Apache 2.0"
-				)
-		),
-		externalDocs = @ExternalDocumentation(
-				description = "EazyBank Loans microservice REST API Documentation"
-		)
-)
+@OpenAPIDefinition(info = @Info(title = "Loans microservice REST API Documentation", description = "Loans microservice REST API Documentation", version = "v1", contact = @Contact(name = "John Doe", email = "john@example.com"), license = @License(name = "Apache 2.0")), externalDocs = @ExternalDocumentation(description = "EazyBank Loans microservice REST API Documentation"))
 public class LoansApplication {
 
 	public static void main(String[] args) {
