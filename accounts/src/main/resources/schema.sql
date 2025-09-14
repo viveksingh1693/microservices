816c324c94a1e4697ce05b84eb8a00db8f4a8a59
vivesing <vivek.singh1694@gmail.com>
2025-09-14 18:06:31 +0530

    [microservices] Adding changes for mysql

--- accounts/src/main/resources/schema.sql
+++ accounts/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `customer` (
-  `customer_id` int AUTO_INCREMENT  PRIMARY KEY,
+  `customer_id` int   PRIMARY KEY,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `mobile_number` varchar(20) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
 
 CREATE TABLE IF NOT EXISTS `accounts` (
   `customer_id` int NOT NULL,
-   `account_number` int AUTO_INCREMENT  PRIMARY KEY,
+   `account_number` int   PRIMARY KEY,
   `account_type` varchar(100) NOT NULL,
   `branch_address` varchar(200) NOT NULL,
   `created_at` date NOT NULL,
