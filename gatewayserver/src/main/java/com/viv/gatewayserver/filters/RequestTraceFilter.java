c5655c921d2f94f1e4be135591b340c44cc6c899
vivesing <vivek.singh1694@gmail.com>
2025-09-20 17:29:45 +0530

    [microservices] Adding the correlation id using request and response filter

---
+++ gatewayserver/src/main/java/com/viv/gatewayserver/filters/RequestTraceFilter.java
@@ -0,0 +1,48 @@
+package com.viv.gatewayserver.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Order(1)
+@Component
+@RequiredArgsConstructor
+public class RequestTraceFilter implements GlobalFilter {
+
+    private  FilterUtility filterUtility;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+        if (isCorrelationIdPresent(requestHeaders)) {
+            log.debug("correlation-id found in RequestTraceFilter : {}",
+                    filterUtility.getCorrelationId(requestHeaders));
+        } else {
+            String correlationID = generateCorrelationId();
+            exchange = filterUtility.setCorrelationId(exchange, correlationID);
+            log.debug("correlation-id generated in RequestTraceFilter : {}", correlationID);
+        }
+        return chain.filter(exchange);
+    }
+
+    private boolean isCorrelationIdPresent(HttpHeaders requestHeaders) {
+        if (filterUtility.getCorrelationId(requestHeaders) != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private String generateCorrelationId() {
+        return java.util.UUID.randomUUID().toString();
+    }
+
+}
---
+++ gatewayserver/src/main/java/com/viv/gatewayserver/filters/ResponseTraceFilter.java
@@ -0,0 +1,32 @@
+package com.viv.gatewayserver.filters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class ResponseTraceFilter {
+
+    FilterUtility filterUtility;
+
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> {
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+                String correlationId = filterUtility.getCorrelationId(requestHeaders);
+                log.debug("Updated the correlation id to the outbound headers: {}", correlationId);
+                exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
+            }));
+        };
+    }
+
+}
